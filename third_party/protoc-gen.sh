8705675b516f094c332e8af3c0b9246551b70e74
Metehan Akbaba <meitesantino@icloud.com>
2020-01-30 15:10:07 +0300

    HTTP REST & Protoc gen swagger gerekli proto dosyalari import edildi

--- third_party/protoc-gen.sh
+++ third_party/protoc-gen.sh
@@ -1,1 +1,3 @@
 protoc --proto_path=api/proto/api --proto_path=third_party --go_out=plugins=grpc:pkg/api/api todo-service.proto
+protoc --proto_path=api/proto/api --proto_path=third_party --grpc-gateway_out=logtostderr=true:pkg/api/api todo-service.proto
+protoc --proto_path=api/proto/api --proto_path=third_party --swagger_out=logtostderr=true:api/swagger/api todo-service.proto
